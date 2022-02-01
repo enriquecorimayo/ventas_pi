ea6432e3b374befdce349410ee4b093efd7185f6
enriquecorimayo <enriquecorimayo2011@gmail.com>
2022-01-31 23:57:46 -0300

    upload produccion

---
+++ pipeline/produccion.py
@@ -0,0 +1,21 @@
+"""
+File Created: 31th January 2022
+Author: Enrique Corimayo 
+-----
+"""
+import pandas as pd
+import pickle
+# Importamos la función transformer para poder usarla en el pipeline
+from pipeline import ft
+# Leemos el archivo json "example.json"
+import json
+with open('C:/Users/54112/source/repos/ventas_pi/notebook/example.json', 'rb') as f:
+    file = json.load(f)
+df = pd.DataFrame([file])
+# Abrimos el pipeline
+with open('C:/Users/54112/source/repos/ventas_pi/pipeline/pipe.pkl','rb')as f:
+    pipe = pickle.load(f)
+# Predicción
+print(pipe.predict(df).round(2))
+
+
